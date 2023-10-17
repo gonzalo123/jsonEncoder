21cb30fa36d0ad4b1f41e97fcc62bbe1397153aa
Gonzalo Ayuso <gonzalo123@gmail.com>
2023-10-17 12:41:41 +0200

    wip

--- jsonEncoder/jsonEncoder.py
+++ jsonEncoder/jsonEncoder.py
@@ -1,15 +1,12 @@
 import decimal
 import json
-import numbers
 from datetime import datetime, date
 
 
 class DefaultEncoder(json.JSONEncoder):
     def default(self, o):
-        if isinstance(o, numbers.Integral):
-            return int(o)
-        if isinstance(o, (decimal.Decimal, numbers.Real)):
-            return float(o)
+        if isinstance(o, decimal.Decimal):
+            return int(o) if o % 1 == 0 else float(o)
         if isinstance(o, datetime):
             return o.__str__()
         if isinstance(o, date):
