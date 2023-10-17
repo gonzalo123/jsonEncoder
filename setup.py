003f47573b700de34ad7f290b15499493f10dbe9
Gonzalo Ayuso <gonzalo123@gmail.com>
2023-10-17 12:42:13 +0200

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup_args = dict(
     name="jsonencoder-gonzalo123",
-    version="1.0.2",
+    version="1.0.3",
     author="Gonzalo Ayuso",
     author_email="gonzalo123@gmail.com",
     description="jsonencoder",
